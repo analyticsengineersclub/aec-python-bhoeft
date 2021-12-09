25aff5dac3ecca31d5e6819d1de80598738ac523
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-12-09 22:09:50 +0000

    initial commit week8 exercises

---
+++ basic_exercises.py
@@ -0,0 +1,16 @@
+# Write a loop that uses while instead of the built-in looping structure
+total_records = 101
+counter = 0
+
+while counter <= total_records:
+    counter += 10
+    print(f"total count: {counter}")
+
+# Write a loop that loop over the keys in a dictionary and prints the values
+
+# Write the functions is_odd and is_even that are shown in the lecture
+
+# Loop over my_first_list and square the value if the value is a number, 
+# and print the calories of the fruit if it’s a fruit (hint: use the dictionary to look up the calories)
+
+# write a function that takes a dictionary as an argument, loops over the keys in the dictionary, prints the square of the value in the value
