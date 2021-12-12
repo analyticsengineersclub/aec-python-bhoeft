761954fa076aa91bdec2bf3851aa975f4294ea6b
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-12-12 05:31:37 +0000

    placeholder tests to fill out

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,27 +4,23 @@ import pytest
 from calc import subtract, multiply, divide
 
 @pytest.fixture
-def normal_inputs():
+def list_2_integers():
     integer1 = 50
     integer2 = 10
     return [integer1, integer2]
 
 @pytest.fixture
-def bad_inputs_list_size_5():
-    """some functions, like subtract(), divide() need lists of exactly
-    2 elements. this fixture will help test those functions for handling
-    > 2 elements.
-    """
+def list_5_integers():
     return [1, 2, 3, 4, 5]
 
 
 class TestsSubtract:
     """test normal, exceptions, and basic edge cases of subtract()"""
 
-    def test_subtract_normal(self, normal_inputs):
+    def test_subtract_normal(self, list_2_integers):
         """check the difference is as expected, when normal inputs are provided"""
         expected_result = 40
-        actual_result = subtract(normal_inputs)
+        actual_result = subtract(list_2_integers)
         assert actual_result == expected_result
 
     def test_subtract_less_than_zero(self):
@@ -33,25 +29,39 @@ class TestsSubtract:
         actual_result = subtract([5, 10])
         assert actual_result == expected_result
 
-    def test_subtract_large_list(self, bad_inputs_list_size_5):
+    def test_subtract_large_list(self, list_5_integers):
         """check that subtract() handles a list input >2 elements by raising
         expected exception"""
-        expected_exception_msg = f"function needs a list of 2. yours has {len(bad_inputs_list_size_5)} elements"
+        expected_exception_msg = f"function needs a list of 2. yours has {len(list_5_integers)} elements"
         with pytest.raises(ValueError) as e:
-            subtract(bad_inputs_list_size_5)
+            subtract(list_5_integers)
         assert e.match(expected_exception_msg)
     
     @pytest.mark.skip
     def test_subtract_empty_list(self):
-        """do nothing yet"""
-
+        pass
 
 @pytest.mark.skip
 class TestsMultiply:
     """test normal, exceptions, and basic edge cases of multiply()"""
 
+    def test_multiply_normal(self, list_2_integers):
+        """check the product is as expected, when normal inputs are provided"""
+    
+    def test_multiply_empty_list(self):
+        pass
 
 @pytest.mark.skip
 class TestsDivide:
     """test normal, exceptions, and basic edge cases of divide()"""
 
+    def test_divide_normal(self, list_2_integers):
+        """check the product is as expected, when normal inputs are provided"""
+        pass
+
+    def test_divide_by_zero(self, list_2_integers):
+        """check the product is as expected, when normal inputs are provided"""
+        pass
+
+    def test_divide_large_list(self, list_5_integers):
+        pass
